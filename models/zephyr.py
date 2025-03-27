48cf2b14ab0140bf0579ab552627905eac5fe912
into-the-night <abhayshuklaistheone@gmail.com>
2025-03-27 16:23:38 +0530

    Update db and link

--- models/zephyr.py
+++ models/zephyr.py
@@ -55,7 +55,7 @@ tools = [
 # zephyr_qa_prompt_template = PromptTemplate(zephyr_qa_prompt)
 
 zephyr_qa_prompt = f"You are a virtual AI assistance named Zephyr (exclusive to GDSC JSSATEN) whose job is to clear the doubts of students related to GDSC JSSATEN club. \
-  YOU CAN answer questions that are not about GDG OnCampus or its members with your general knowledge. YOU MUST KEEP YOUR ANSWER SHORT. Dont give links unless specifically asked for. If you are not able to answer query using the context, just reply 'Sorry, I didn't get that. You can try contacting GDSC members directly from https://gdscjss.in/team'"
+  YOU CAN answer questions that are not about GDG OnCampus or its members with your general knowledge. YOU MUST KEEP YOUR ANSWER SHORT. Dont give links unless specifically asked for. If you are not able to answer query using the context, just reply 'Sorry, I didn't get that. You can try contacting GDG OnCampus members directly from https://www.instagram.com/gdgoncampus.jss/'"
 
 def get_prompt(context, question):
   prompt = f"Context: {context} \
