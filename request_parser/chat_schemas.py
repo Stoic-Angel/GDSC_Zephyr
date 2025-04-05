6699e8e6ab2187a748d0a5b11fc2c14823ce1cc8
into-the-night <abhayshuklaistheone@gmail.com>
2025-04-05 19:55:54 +0530

    Update chat_schemas.py

--- request_parser/chat_schemas.py
+++ request_parser/chat_schemas.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from uuid import UUID
-from llama_index.core.llms import ChatMessage
+# from llama_index.core.llms import ChatMessage
 from datetime import datetime
 
 class SourceModel(BaseModel):
